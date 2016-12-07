307af7b3a06efe5a728fb1d2e3d180529b1df65d
Adam Derewecki <derewecki@gmail.com>
2016-12-06 21:20:11 -0800

    Now /face/{count,squares}

--- app/face_detect.py
+++ app/face_detect.py
@@ -6,27 +6,30 @@ class FaceDetect:
     def __init__(self, image_path):
         self.image_path = image_path
 
-    def perform(self):
         casc_path = "classifiers/haarcascade_frontalface_default.xml"
         face_cascade = cv2.CascadeClassifier(casc_path)
 
-        image = cv2.imread(self.image_path)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        self.image = cv2.imread(self.image_path)
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
-        faces = face_cascade.detectMultiScale(
+        self.faces = face_cascade.detectMultiScale(
             gray,
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30),
             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
         )
+        print("Found {0} faces!".format(len(self.faces)))
 
-        print("Found {0} faces!".format(len(faces)))
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    def face_count(self):
+        return dict(face_count=len(self.faces))
+
+    def face_squares(self):
+        for (x, y, w, h) in self.faces:
+            cv2.rectangle(self.image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         file_path = "public/%s" % self.image_path
         print "Writing to: %s" % file_path
-        cv2.imwrite(file_path, image)
-        return dict(face_count=len(faces))
+        cv2.imwrite(file_path, self.image)
+        return cv2.imencode('.jpg', self.image)
--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
 import sys
+from io import BytesIO
 
 import flask
 
@@ -6,10 +7,16 @@ from face_detect import FaceDetect
 
 app = flask.Flask(__name__)
 
-@app.route('/face', methods=['PUT'])
+@app.route('/face/count', methods=['PUT'])
 def face():
     image = flask.request.files['image']
-    return flask.jsonify(FaceDetect(image.filename).perform())
+    return flask.jsonify(FaceDetect(image.filename).face_count())
+
+@app.route('/face/squares', methods=['PUT'])
+def face_squares():
+    image = flask.request.files['image']
+    _, out_image, _ = FaceDetect(image.filename).face_squares()
+    return flask.send_file(BytesIO(out_image))
 
 if __name__ == '__main__':
     app.run()
